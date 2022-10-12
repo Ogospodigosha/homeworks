31d6ac50827843bfa058175a32763066a39c555d
Ogospodigosha <odintsovis@hotmail.com>
2022-10-12 22:23:16 +0300

    HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+type SortType ={
+    type: 'sort'
+    payload: 'up'| 'down'
+}
+type CheckType = {
+    type: 'check'
+    payload: 18
+}
+type ActionType = SortType | CheckType
+
+export const homeWorkReducer = (state: UserType, action: ActionType): UserType => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+           if (action.payload === 'up') {
+               return [...state].sort((a,b)=>a.name > b.name ? 1: -1)
+           } else {
+               return [...state].sort((a,b)=>b.name > a.name ? 1: -1)
+           }
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(el => el.age>= 18)
         }
         default: return state
     }
