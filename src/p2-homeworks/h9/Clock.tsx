5d675ad558d38ada924d47e7a6f7ed35393ce001
Ogospodigosha <odintsovis@hotmail.com>
2022-10-19 19:36:45 +0300

    HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,40 @@
-import React, {useState} from 'react'
+import React, {MouseEvent, useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+       clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+
+          setDate(new Date())
+
+
         }, 1000)
         setTimerId(id)
     }
-
-    const onMouseEnter = () => {
-        // show
+    let years = date.getFullYear().toString()
+    let month = date.getMonth().toString()
+    let day = date.getDay().toString()
+    let hours = date.getHours()
+    let minutes = new Date().getMinutes().toString()
+    let seconds = new Date().getSeconds().toString()
+    const onMouseEnter = (e: MouseEvent<HTMLDivElement>) => {
+        setShow(true)
     }
-    const onMouseLeave = () => {
-        // close
+    const onMouseLeave = (e: MouseEvent<HTMLDivElement>) => {
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = hours + ":" + minutes + ":" + seconds // fix with date
+    const stringDate = years + ":" +"0"+ month + ":" + "0"+day
 
     return (
         <div>
