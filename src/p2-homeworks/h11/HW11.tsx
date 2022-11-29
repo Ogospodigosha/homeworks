5cc51388369ac1e96eb2eea4595f879ffcfe9540
Ogospodigosha <odintsovis@hotmail.com>
2022-11-29 23:11:30 +0300

    hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -2,9 +2,19 @@ import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
+
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+
+    const min = 0
+    const max = 100
+
+    const [value1, setValue1] = useState<number>(min)
+    const [value2, setValue2] = useState<number>(max)
+
+    const onChangeRange = ([value1, value2]: [number, number]) => {
+        setValue1(value1)
+        setValue2(value2)
+    }
 
     return (
         <div>
@@ -13,25 +23,32 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <div style={{display: 'inline-block', width: '25px', textAlign: 'center'}}>{value1}</div>
+                <div style={{display: 'inline-block', width: '300px'}}>
+                    <SuperRange onChangeRange={setValue1}
+                                onChangeRange2={setValue2}
+                                value1={value1}
+                                value2={value2}
+                    />
+                </div>
+
             </div>
 
             <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
+                <div style={{display: 'inline-block', width: '25px', textAlign: 'center'}}>{value1}</div>
+                <div style={{display: 'inline-block', width: '300px'}}>
+                    <SuperDoubleRange onChangeRange={onChangeRange}
+                                      value={[value1, value2]}
+                    />
+                </div>
+                <div style={{
+                    display: 'inline-block',
+                    width: '25px',
+                    textAlign: 'center',
+                    padding:'5px'
+                }}>   {value2}</div>
             </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
         </div>
     )
 }
